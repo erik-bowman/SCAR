9e4e6e912ee769b20d57cbb1cdb7921a1058b603
Erik Bowman <erik.bowman@icsinc.com>
2017-05-20 13:51:34 -0500

    RHEL-06-000227 bugfix

--- lib/Redhat/6/000227.pm
+++ lib/Redhat/6/000227.pm
@@ -35,7 +35,7 @@ use strict;
 use warnings FATAL => 'all';
 
 # Scar modules
-use Scar qw( parse_file );
+use Scar;
 use Scar::Util::Log;
 use Scar::Util::Backup;
 
@@ -51,10 +51,20 @@ sub new {
 
 sub check {
     my ($self) = @_;
-    if ( ingest_file( '/etc/ssh/sshd_config', '^Protocol\W+2$' ) ) {
-        $self->_set_finding_status('NF');
+    if ( !defined $self->{parent}->{files}->{'/etc/ssh/sshd_config'}
+        ->{Protocol}[1] )
+    {
+        if (defined $self->{parent}->{files}->{'/etc/ssh/sshd_config'}
+            ->{Protocol}[0] )
+        {
+            if ( $self->{parent}->{files}->{'/etc/ssh/sshd_config'}
+                ->{Protocol}[0] eq '2' )
+            {
+                $self->_set_finding_status('NF');
+            }
+        }
     }
-    else {
+    if ( !defined $self->get_finding_status() ) {
         $self->_set_finding_status('O');
     }
     return $self;
