241e8c5ede681fb7b307f69e3089480dabe69798
Erik Bowman <erik.bowman@icsinc.com>
2017-05-20 09:54:35 -0500

    RHEL-06-000039 Create Check

--- lib/Redhat/6/000039.pm
+++ lib/Redhat/6/000039.pm
@@ -36,7 +36,7 @@ use strict;
 use warnings FATAL => 'all';
 
 # Scar modules
-use Scar;
+use Scar qw{ get_file_owner };
 use Scar::Util::Log;
 use Scar::Util::Backup;
 
@@ -52,7 +52,12 @@ sub new {
 
 sub check {
     my ($self) = @_;
-
+    if (get_file_owner('/etc/passwd') eq '0') {
+        $self->_set_finding_status('NF');
+    }
+    else {
+        $self->_set_finding_status('O');
+    }
     return $self;
 }
 
