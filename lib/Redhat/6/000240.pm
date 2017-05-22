be6f65f29de7fd9b03c92ad145430e578327c792
Erik Bowman <erik.bowman@icsinc.com>
2017-05-22 00:23:56 -0500

    RHEL-06-000240 Create Check

--- lib/Redhat/6/000240.pm
+++ lib/Redhat/6/000240.pm
@@ -20,7 +20,6 @@
 # RULE TITLE
 #   The SSH daemon must be configured with the Department of Defense (DoD) login banner.
 #
-# TODO: Create Check
 # TODO: Create Remediation
 #
 # AUTHOR
@@ -45,14 +44,21 @@ our $VERSION = 0.01;
 
 sub new {
     my ( $class, $parent ) = @_;
-    my $self = bless { parent => $parent }, $class;
+    my $self = bless \%{$parent}, $class;
 
     return $self;
 }
 
 sub check {
     my ($self) = @_;
-
+    if ( defined $self->{sshd_config}->{Banner} ) {
+        if ( $self->{sshd_config}->{Banner} eq '/etc/issue' ) {
+            $self->_set_finding_status('NF');
+        }
+    }
+    if ( !defined $self->get_finding_status() ) {
+        $self->_set_finding_status('O');
+    }
     return $self;
 }
 
